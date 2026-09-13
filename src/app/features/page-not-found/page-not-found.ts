6d45bd828f58d4cf4ca035e05c180c381fffb617
anaeltech <anaelpayetpro@gmail.com>
2026-09-13 19:41:35 +0200

    perf(page-not-found): use OnPush change detection

--- src/app/features/page-not-found/page-not-found.ts
+++ src/app/features/page-not-found/page-not-found.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
   imports: [],
   templateUrl: './components/page-not-found.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PageNotFoundComponent {}
